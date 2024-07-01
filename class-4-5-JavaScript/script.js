180edd664c422b46220ad3b7353c8170828074f4
Diana Castaño <dianaczapata@gmail.com>
2024-07-01 13:21:32 -0500

    update/fix console.log inside the functions and add try/catch in sqrRoot function

--- class-4-5-JavaScript/script.js
+++ class-4-5-JavaScript/script.js
@@ -4,37 +4,37 @@ const negativeArray=[-1,-7,-8,-99,-25,-30]
 
 //Testing positiveSum Kata exercise
 const positiveSum=(arr) => {
-  let initialValue=0
-  let positiveList= arr.filter(number => number >=0)
+  const initialValue=0
+  const positiveList= arr.filter(number => number >=0)
   const totalSum = positiveList.reduce(
   (accumulator, currentValue) => accumulator + currentValue,
   initialValue,
 );
-  return console.log(totalSum)
+  return totalSum
 }
 
 //Testing goals function from Kata exercise
 const goals= (laLigaGoals, copaDelReyGoals, championsLeagueGoals)=> {
-  return console.log(laLigaGoals + copaDelReyGoals +championsLeagueGoals)
+  return (laLigaGoals + copaDelReyGoals +championsLeagueGoals)
 }
 
 //Arithmetic operations
 const sum = (a,b)=>{
- return console.log('El total de la suma es:',a + b)
+ return ('El total de la suma es:',a + b)
 }
 
 const sub = (a,b)=>{
- return console.log('El total de la resta es:', a - b)
+ return ('El total de la resta es:', a - b)
 }
 
 const mult = (a,b)=>{
- return console.log('El total de la multiplicación es:', a * b)
+ return ('El total de la multiplicación es:', a * b)
 }
 
 const div= (a,b)=>{
  try{
   if(b !== 0){
-   return console.log('El total de la división es:', a / b)
+   return ('El total de la división es:', a / b)
   }else{
    throw new Error('Ni Thanos puede dividir por cero 👿🦹‍♂️')
   }
@@ -46,26 +46,38 @@ const div= (a,b)=>{
 }
 
 const mod=(a,b)=>{
- return console.log('El total del módulo es de:',a % b)
+ return ('El total del módulo es de:',a % b)
 }
 
 const sqrRoot= (a)=>{
- return console.log(`El total de la raiz cuadrada de ${a} es: ${Math.sqrt(a)}`)
+   try{
+  if(a >= 0){
+   return (`El total de la raiz cuadrada de ${a} es: ${Math.sqrt(a)}`)
+  }else{
+   throw new Error('No existe magia capaz de encontrar una raiz negativa 🧙‍♀️')
+  }
+ }catch (error){
+   console.log(`Oops, algo salió mal. ${error.message}`)
+ } finally{
+   console.log('Operación finalizada')
+ }
+
 }
 
 const exponentiation= (a,b)=>{
- return console.log(`El total de ${a} elevado a la ${b} es de: ${a ** b}`)
+ return (`El total de ${a} elevado a la ${b} es de: ${a ** b}`)
 }
 
-sum(8,9)
-sub(-20,2)
-mult(8,5)
-div(4,10)
-div(5,0)
-mod(6,3)
-sqrRoot(7)
-exponentiation(3,4)
+//console.log(sum(8,9))
+//console.log(sub(-20,2))
+//console.log(mult(8,5))
+//console.log(div(4,10))
+//console.log(div(5,0))
+//console.log(mod(6,3))
+//console.log(sqrRoot(7))
+console.log(sqrRoot(-7))
+//console.log(exponentiation(3,4))
 
-positiveSum(exampleArray)
-positiveSum(negativeArray)
-goals(43, 10, 5)
+//console.log(positiveSum(exampleArray))
+//console.log(positiveSum(negativeArray))
+//console.log(goals(43, 10, 5))
